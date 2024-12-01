910f6690115f7b4bd132ac6cc980bd4b614a116c
swatisahu1305 <sketchinggrowth163@gmail.com>
2024-12-01 23:03:28 +0530

    first commit

---
+++ javascript start/main.js
@@ -0,0 +1,109 @@
+// var x = 100;
+// var y = 100;
+// console.log(x+y);
+// console.log(x-y);
+// console.log(x*y);
+// console.log(x/y);
+// console.log(x%y);
+
+
+
+// console.log (5*2 / 5- 50+2/2 );
+// var firstname = "petter"
+// var lastname = "parker"
+// console.log ( firstname +  " " + lastname);
+
+// var firstname = "swati"
+// var lastname = "sahu"
+// console.log (firstname + lastname);
+
+// var x = "250";
+// var y =100;
+// console.log(x+y);
+// console.log(x-y);
+// console.log(x*y);
+// console.log(x/y);
+// // console.log(x%y);
+
+// var x = 50;
+// var y = "komal";
+// if(x>y){
+//     console.log("x");
+// }
+// else if (x<y){
+//     console.log("y");
+// }
+// else if(x==y){
+//     console.log("xy");
+// }
+// else{
+//     console.log("something went wrong");
+// }
+// var num= 3;
+// if(num%2 ===0){
+//     console.log("the number is even");
+// }
+//  else{
+//     console.log("the number is odd");
+// }
+
+//functions in javascript
+// regular function
+
+// add two number
+// function addtwonumber(a,b){
+//     return  a+b;
+
+// }
+// console.log(addtwonumber(7,5));
+
+
+// even or odd
+// function checknum(num){
+//     if(num%2===0){
+//         return "even";
+//     }
+//     else{
+//         return "odd";
+//     }
+
+// }
+//  console.log(checknum(6));
+
+// celciusto farenheight
+// function celiciustofarenheight (celcius){
+//     return celcius*9/5+32;
+// }
+//  console.log(celiciustofarenheight(30));
+
+// arrow functio in javascript
+// add two number
+
+// const add=(a,b)=>{
+// console.log (a+b);
+// }
+// add(6,6);
+
+// const checknum =(num)=>{
+// if(num%2===0){
+//     console.log("the number is even");
+// }
+// else{
+//     console.log("the number is odd");
+// }
+// }
+// console.log(checknum(9));
+
+// const squre =(number) => {
+//     return number*number;
+
+// }
+// console.log(squre(25));
+
+// const celciustofarenheigh=(celcius)=>{
+// return celcius*9/5+32;
+// }
+// console.log(celciustofarenheigh(25));
+
+
+// array in javascript
